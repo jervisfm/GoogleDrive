3569aca409c02b0cc2b4a2761a6a7c17c9cddb8c
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-11-19 07:40:40 +0000

    syntax error format fix

--- gdrive.py
+++ gdrive.py
@@ -23,23 +23,22 @@ class GDrive(object):
         pass
     
     def upload(src_file, dest_path=None):
-    """Uploads the 'src_file' to the destination file.
+        """Uploads the 'src_file' to the destination file.
 
-    Args:
-      src_file: the source file to be uploaded.
-      dest_path: the destination folder path
-    """
+        Args:
+        src_file: the source file to be uploaded.
+        dest_path: the destination folder path."""
         pass
 
     def download(src_file, dest_file):
-    """Downloads the specified file from Drive onto a local file. 
-    Args:
-      src_file: the source file to be uploaded
-      dest_file: the destination file to save downloaded file to. 
-    """
+        """Downloads the specified file from Drive onto a local file. 
+        Args:
+          src_file: the source file to be uploaded
+          dest_file: the destination file to save downloaded file to. 
+        """
         pass
 
     def list(file_path): 
-    """Lists files in the given path."""
+        """Lists files in the given path."""
         pass
 
